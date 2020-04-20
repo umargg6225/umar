66a823b9b04c7ebb88ec2a8b4c5581199abf6b6e
umargg6225 <63993264+umargg6225@users.noreply.github.com>
2020-04-20 10:52:38 +0500

    Update and rename zeroboy.py to Umar.py

--- Umar.py
+++ Umar.py
@@ -49,17 +49,17 @@ def jalan(z):
 ##### LOGO #####
 logo = """ -----------------------------•◈•
 (  __)\\ ____--------------_------------•◈•
-|__(~)    •||•THE - AAHIL -OFFICAL------•◈•
-|__\~~) •||•RANA - RAJPUT---------------•◈•
-|__(-----\  •◈•------BLACK-TIGER--------•◈•
+|__(~)    •||•THE - PRINCE -OFFICAL------•◈•
+|__\~~) •||•PRINCE - UMAR---------------•◈•
+|__(-----\  •◈•------BLACK-SHADOW--------•◈•
 |__~~~\ •◈•-----█-------⑦-------█------•◈•
 |__~~~\ •◈•-----█-------⑧-------█------•◈•
 |__~~~\ •◈•-----█-------⑥-------█------•◈•
 \033[1;91m=======================================
-\033[1;96mAuthor  \033[1;93m: \033[1;92mRana Aahil
-\033[1;96mInstagram \033[1;93m: \033[1;FlowRana
-\033[1;96mFacebook  \033[1;93m: \033[1; Aahilrna4072
-\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/Therana/zero
+\033[1;96mAuthor  \033[1;93m: \033[1;92PRINCE UMAR
+\033[1;96mInstagram \033[1;93m: \033[1;😍
+\033[1;96mFacebook  \033[1;93m: \033[1;https://www.facebook.com/profile.php?id=100041739195653
+\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/princeumar/umar
 \033[1;91m======================================="""
 
 def tik():
@@ -80,15 +80,15 @@ vuln = "\033[32mVuln"
 os.system("clear")
 print "\033[1;96m ============================================================="
 print  """\033[1;91m=======================================
-\033[1;96mAuthor  \033[1;93m: \033[1;92mRana Aahil
-\033[1;96mInstagram \033[1;93m: \033[1;92mFlowRana
-\033[1;96mFacebook  \033[1;93m: \033[1;92m Aahilrana4072
-\033[1;96mGithub \033[1;93m: \033[1;92mhttps://Github.com/Therana/zero
+\033[1;96mAuthor  \033[1;93m: \033[1;92PRINCE UMAR
+\033[1;96mInstagram \033[1;93m: \033[1;92😍
+\033[1;96mFacebook  \033[1;93m: \033[1;92mhttps://www.facebook.com/profile.php?id=100041739195653
+\033[1;96mGithub \033[1;93m: \033[1;92mhttps://Github.com/princeumar/umar
 \033[1;91m======================================="""
 print " \x1b[1;93m============================================================="
 
-CorrectUsername = "rana"
-CorrectPassword = "rana"
+CorrectUsername = "umar"
+CorrectPassword = "umar"
 
 loop = 'true'
 while (loop == 'true'):
@@ -100,10 +100,10 @@ while (loop == 'true'):
             loop = 'false'
         else:
             print "Wrong Password"
-            os.system('xdg-open https://www.Youtube.com/UCsdJQbRf0xpvwaDu1rqgJuA')
+            os.system('xdg-open https://www.Facebook.com/https://www.facebook.com/profile.php?id=100041739195653')
     else:
         print "Wrong Username"
-        os.system('xdg-open https://www.Youtube.com/UCsdJQbRf0xpvwaDu1rqgJuA')
+        os.system('xdg-open https://www.Youtube.com/https://www.facebook.com/profile.php?id=100041739195653')
 
 def login():
 	os.system('clear')
